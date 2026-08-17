e875e440c0ecb78eebd17678b860b9f82801323c
Lianna <liannapyman@gmail.com>
2026-08-17 14:50:32 +0930

    Refactor tuner components to improve state management and styling; update string data format

--- components/tuner/Strings.tsx
+++ components/tuner/Strings.tsx
@@ -1,15 +1,20 @@
+import { Dispatch, SetStateAction } from "react";
+
 type StringsProps = {
     stringName: string;
     octave: number;
     hertz: number;
+    isActive: boolean;
+    setIsActive: Dispatch<SetStateAction<String>>;
+    stringId: string;
 }
 
-const Strings: React.FunctionComponent<StringsProps> = ({stringName, octave, hertz}) => {
-    // Reusable component
+const Strings: React.FunctionComponent<StringsProps> = ({stringName, octave, hertz, isActive, setIsActive, stringId}) => {
+    
     return (
-        <div className="flex justify-between rounded-xl items-center py-2.5 px-3.5 text-bright-white bg-transparent active:string-active cursor-pointer">
+        <div onClick={() => setIsActive(stringId)} className={"flex justify-between rounded-xl items-center py-2.5 px-3.5 text-bright-white bg-transparent cursor-pointer" + (isActive ? " active-string" : "")}>
             <div className="flex items-baseline gap-x-1">
-                <p className="text-xl">{stringName}</p>
+                <p className="text-xl font-bold">{stringName}</p>
                 <p className="text-xs">{octave}</p>
             </div>
             <div>
--- components/tuner/StringsWrapper.tsx
+++ components/tuner/StringsWrapper.tsx
@@ -1,7 +1,27 @@
-const StringsWrapper = () => {
+import { useState } from "react";
+import strings from "../../data/strings.json";
+import Strings from "./Strings";
+
+type StringsWrapperProps = {
+    selectedInstrument: string;
+}
+
+const StringsWrapper: React.FunctionComponent<StringsWrapperProps> = ({selectedInstrument}) => {
+    const stringsInfo = strings[selectedInstrument as keyof typeof strings];
+    const [isActive, setIsActive] = useState("");
+    const activeStringId = isActive || `${stringsInfo[0].stringName}${stringsInfo[0].octave}`
     return (
         <div>
-            <p>Hello, strings wrapper.</p>
+            {
+                stringsInfo.map((string) => {
+                    const stringId = `${string.stringName}${string.octave}`
+                    return (
+                    <Strings key={`${selectedInstrument}${stringId}`} stringId={stringId} isActive={stringId === activeStringId} setIsActive={setIsActive} stringName={string.stringName} octave={string.octave} hertz={string.hertz} />
+                )
+            })
+            }
         </div>
     )
 }
+
+export default StringsWrapper;
